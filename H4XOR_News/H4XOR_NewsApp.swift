877c51e93f8a2fb85d95aa15fef4fd2f3cb3b0a3
許君愷 / xu jun kai <kevin16021777@gmail.com>
2024-06-21 15:31:42 +0800

    Initial Commit

---
+++ H4XOR_News/H4XOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NewsApp.swift
+//  H4XOR_News
+//
+//  Created by 許君愷 on 2024/6/21.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
